b08fe7aad661d4e24c2db2add82d7b8688cc2bfc
Yuma Matsuoka <a.iebako.yuma@gmail.com>
2016-08-16 10:33:41 +0900

    add answer 1-1-9

---
+++ 1_warming-up/1_tbd1/009.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""
+05:
+"""
+
+# python2とpython3のprint文の互換用おまじない
+from __future__ import print_function
+import six
+
+n_list = []
+for i in six.moves.range(1, 101):
+    n_list.append(i)
+
+#debug
+print(n_list)
