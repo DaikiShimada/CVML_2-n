e41b4e4f49787eac71273afbfc98c80f13674eb3
Yuma Matsuoka <a.iebako.yuma@gmail.com>
2016-08-16 10:34:14 +0900

    add answer 1-1-12

---
+++ 1_warming-up/1_tbd1/012.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""
+08:
+"""
+
+# python2とpython3のprint文の互換用おまじない
+from __future__ import print_function
+
+#from '1-1-6'
+n_nlist = [i for i in range(1, 101)]
+
+for i in n_nlist:
+    print(i, end=" ")
+
+"""
+python3からはprint文がprint関数になったので、
+print()の最後にendを入れることで改行を防ぐことができる
+"""
