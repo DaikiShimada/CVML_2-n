dcb477e16f71baf4a1901afc8c73d7c862ab31c9
Yuma Matsuoka <a.iebako.yuma@gmail.com>
2016-08-16 10:35:11 +0900

    add answer 1-1-18

---
+++ 1_warming-up/1_tbd1/018.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""
+14:
+"""
+
+# python2とpython3のprint文の互換用おまじない
+from __future__ import print_function
+
+def calc(a, b):
+    c = a + b
+    d = a - b
+
+    return (c, d)
+
+
+print("返値=", calc(3, 2)[0])
+
+# tips
+c, d = calc(3, 2)
+print("返値=", d)
